08bcaa53349d601d54fb64f44cafa74ea9497d49
Arne Güldener <arne-@hotmail.de>
2023-12-01 16:18:44 +0100

    2nd part now working too

--- 2023/first/evaluator.py
+++ 2023/first/evaluator.py
@@ -13,22 +13,111 @@ class TestDataEvaluatorPart1(object):
 
     @classmethod
     def evaluate_line(cls, line):
-        left_char = cls.get_left_char_digit(line)
-        right_char = cls.get_right_char_digit(line)
+        left_char = cls.get_leftest_number(line)[1]
+        right_char = cls.get_rightest_number(line)[1]
         no = int(left_char + right_char)
         return no
 
-    @staticmethod
-    def get_left_char_digit(line):
-        for char in line:
-            if str.isdigit(char):
-                return char
-        raise Exception("Did not find a digit")
+    @classmethod
+    def get_leftest_number(cls, line):
+        return cls.get_left_char_digit(line)
 
-    @staticmethod
-    def get_right_char_digit(line):
-        for char in line[::-1]:
-            if str.isdigit(char):
-                return char
-        raise Exception("Did not find a digit")
+    @classmethod
+    def get_rightest_number(cls, line):
+        return cls.get_right_char_digit(line)
 
+    @classmethod
+    def get_left_char_digit(cls, line: str):
+        for index, char in enumerate(line):
+            if str.isdigit(char):
+                return index, char
+        raise ValueError("Did not find a digit")
+
+    @classmethod
+    def get_right_char_digit(cls, line: str):
+        for index, char in enumerate(line[::-1]):
+            if str.isdigit(char):
+                index = len(line) - 1 - index  # Count backwards, but index is from front
+                return index, char
+        raise ValueError("Did not find a digit")
+
+
+class TestDataEvaluatorPart2(TestDataEvaluatorPart1):
+    class Digits(IntEnum):
+        one = 1
+        two = 2
+        three = 3
+        four = 4
+        five = 5
+        six = 6
+        seven = 7
+        eight = 8
+        nine = 9
+
+    @classmethod
+    def get_leftest_number(cls, line):
+        leftest_word, leftest_char = None, None
+        try:
+            leftest_word = cls.get_left_word_digit(line)
+        except IndexError:
+            pass  # Let's hope leftest char has some good news
+        try:
+            leftest_char = cls.get_left_char_digit(line)
+        except ValueError:  # If there is no digit
+            pass
+
+        if leftest_word is None and leftest_char is None:
+            raise Exception("Something's wrong. Didn't find anything")
+        elif leftest_char is None:
+            return leftest_word
+        elif leftest_word is None:
+            return leftest_char
+        elif leftest_word[0] < leftest_char[0]:
+            return leftest_word
+        else:
+            return leftest_char
+
+    @classmethod
+    def get_rightest_number(cls, line):
+        rightest_word, rightest_char = None, None
+        try:
+            rightest_word = cls.get_right_word_digit(line)
+        except IndexError:
+            pass  # Lets' hope rightest char has some good news
+        try:
+            rightest_char = cls.get_right_char_digit(line)
+        except ValueError:  # If there is no digit
+            pass
+
+        if rightest_word is None and rightest_char is None:
+            raise Exception("Something's wrong. Didn't find anything")
+        elif rightest_char is None:
+            return rightest_word
+        elif rightest_word is None:
+            return rightest_char
+        elif rightest_word[0] > rightest_char[0]:
+            return rightest_word
+        else:
+            return rightest_char
+
+    @classmethod
+    def find_digit_words(cls, line, direction_left):
+        digit_index = []
+        for digit in cls.Digits:
+            find_method = line.find if direction_left else line.rfind
+            find_idx = find_method(digit.name)
+            if find_idx >= 0:
+                digit_index.append((find_idx, str(digit.value)))
+        return digit_index
+
+    @classmethod
+    def get_left_word_digit(cls, line: str):
+        digit_words = cls.find_digit_words(line, direction_left=True)
+        first_digit = sorted(digit_words, key=lambda x: x[0])[0]
+        return first_digit
+
+    @classmethod
+    def get_right_word_digit(cls, line: str):
+        digit_words = cls.find_digit_words(line, direction_left=False)
+        last_digit = sorted(digit_words, key=lambda x: x[0])[-1]
+        return last_digit
--- 2023/first/test_data.py
+++ 2023/first/test_data.py
@@ -30,3 +30,52 @@ class TestPart1:
         result = evaluator.evaluate_calibration_data()
         print("Result is", result)
         assert result == 56506
+
+
+class TestPart2:
+    test_data_part_2 = ["two1nine",
+                        "eightwothree",
+                        "abcone2threexyz",
+                        "xtwone3four",
+                        "4nineeightseven2",
+                        "zoneight234",
+                        "7pqrstsixteen"]
+
+    @staticmethod
+    @pytest.mark.parametrize(("line", "expected_result"), ([test_data_part_2[0], 29],
+                                                           [test_data_part_2[1], 83],
+                                                           [test_data_part_2[2], 13],
+                                                           [test_data_part_2[3], 24],
+                                                           [test_data_part_2[4], 42],
+                                                           [test_data_part_2[5], 14],
+                                                           [test_data_part_2[6], 76]))
+    def test_single_lines(line, expected_result):
+        assert TestDataEvaluatorPart2.evaluate_line(line) == expected_result
+
+    @staticmethod
+    @pytest.mark.parametrize(("line", "expected_result"), (#["12", 12],
+                                                           #["onetwo", 12],
+                                                           #["1two1", 11],
+                                                           #["eightwothree.", 83],
+                                                           #["sevenine", 79],
+                                                           #["3lsevenonebpgfgonethreeeightwos", 32],
+                                                           #["898xyxchoasuhz", 88],
+                                                           ["1cf", 11],
+                                                           ["four4rjrkzvfive2cfl7fourfive", 45]))
+    def test_custom_lines(line, expected_result):
+        assert TestDataEvaluatorPart2.evaluate_line(line) == expected_result
+
+    @classmethod
+    def test_evaluate_result(cls):
+        evaluator = TestDataEvaluatorPart2(cls.test_data_part_2)
+        result = evaluator.evaluate_calibration_data()
+        assert result == 281
+
+    @staticmethod
+    def test_all_test_data():
+        with open("./test_data.txt", "r") as file:
+            lines = file.readlines()
+        evaluator = TestDataEvaluatorPart2(lines)
+        result = evaluator.evaluate_calibration_data()
+        print("Result is", result)
+        assert result == 56017
