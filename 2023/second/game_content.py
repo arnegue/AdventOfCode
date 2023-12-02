a548787f3c03a5058b332102514d6b724af88c6f
Arne Güldener <arne-@hotmail.de>
2023-12-02 13:12:04 +0100

    2P1: Parsing possible

---
+++ 2023/second/game_content.py
@@ -0,0 +1,81 @@
+import enum
+import re
+
+
+def get_number_from_string(number_string: str) -> int:
+    numbers = re.findall(r'\d+', number_string)
+    if 0 < len(numbers) > 1:
+        raise Exception(f"Did not find 1 number in \"{number_string}")
+    return int(numbers[0])
+
+
+class Color(enum.Enum):
+    red = enum.auto()
+    blue = enum.auto()
+    green = enum.auto()
+
+    @classmethod
+    def get_color_from_string(cls, color_string):
+        for color in cls:
+            if color.name in color_string:
+                return color
+        raise ValueError(f"Did not find any color in string \"{color_string}\"")
+
+
+class Cube(object):
+    def __init__(self, color: Color):
+        self.color = color
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.color == other.color
+        else:
+            return False
+
+
+class SetCubes(object):
+    def __init__(self, amount_cube_colors: list[tuple[int, Cube]]):
+        self.amount_cube_colors = amount_cube_colors
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.amount_cube_colors == other.amount_cube_colors
+        else:
+            return False
+
+
+class Game(object):
+    def __init__(self, game_id: int, list_sets: list[SetCubes]):
+        self.game_id = game_id
+        self.list_sets = list_sets
+
+    @classmethod
+    def parse_string(cls, string):
+        # "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+        game_id_string, set_strings = string.split(":")  # ["Game 1", " 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"]
+        game_id = get_number_from_string(game_id_string)
+
+        set_strings = set_strings.split(";")  # --> [" 3 blue, 4 red", " 1 red, 2 green", " 6 blue; 2 green"]
+        list_sets = []
+        for set_ in set_strings:
+            cube_strings = set_.split(",")  # --> [" 3 blue", " 4 red"]
+            list_cubes = []
+            for cube_string in cube_strings:
+                amount_cubes = get_number_from_string(cube_string)
+                cube_color = Color.get_color_from_string(cube_string)
+                list_cubes.append((amount_cubes, Cube(cube_color)))
+            set_cubes = SetCubes(list_cubes)
+            list_sets.append(set_cubes)
+        return Game(game_id, list_sets)
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            if self.game_id == other.game_id:
+                if len(self.list_sets) == len(other.list_sets):
+                    for set_idx, set_ in enumerate(self.list_sets):
+                        other_set = other.list_sets[set_idx]
+                        if set_ != other_set:
+                            return False
+                    return True
+
+        return False
---
+++ 2023/second/test_data.py
@@ -0,0 +1,37 @@
+import pytest
+
+from game_content import Game, Color, SetCubes, Cube
+
+
+class TestPart1(object):
+    test_data = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n" \
+                "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n" \
+                "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n" \
+                "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n" \
+                "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green\n"
+    split_data = test_data.splitlines(keepends=False)
+
+    @classmethod
+    @pytest.mark.parametrize(("test_line", "expected_results"), ([split_data[0], Game(1, [SetCubes([(3, Cube(Color.blue)), (4, Cube(Color.red))]),
+                                                                                          SetCubes([(1, Cube(Color.red)), (2, Cube(Color.green)), (6, Cube(Color.blue))]),
+                                                                                          SetCubes([(2, Cube(Color.green))]),
+                                                                                          ])],
+                                                                 [split_data[1], Game(2, [SetCubes([(1, Cube(Color.blue)), (2, Cube(Color.green))]),
+                                                                                          SetCubes([(3, Cube(Color.green)), (4, Cube(Color.blue)), (1, Cube(Color.red))]),
+                                                                                          SetCubes([(1, Cube(Color.green)), (1, Cube(Color.blue))]),
+                                                                                          ])],
+                                                                 [split_data[2], Game(3, [SetCubes([(8, Cube(Color.green)), (6, Cube(Color.blue)), (20, Cube(Color.red))]),
+                                                                                          SetCubes([(5, Cube(Color.blue)), (4, Cube(Color.red)), (13, Cube(Color.green))]),
+                                                                                          SetCubes([(5, Cube(Color.green)), (1, Cube(Color.red))]),
+                                                                                          ])],
+                                                                 [split_data[3], Game(4, [SetCubes([(1, Cube(Color.green)), (3, Cube(Color.red)), (6, Cube(Color.blue))]),
+                                                                                          SetCubes([(3, Cube(Color.green)), (6, Cube(Color.red))]),
+                                                                                          SetCubes([(3, Cube(Color.green)), (15, Cube(Color.blue)), (14, Cube(Color.red))]),
+                                                                                          ])],
+                                                                 [split_data[4], Game(5, [SetCubes([(6, Cube(Color.red)), (1, Cube(Color.blue)), (3, Cube(Color.green))]),
+                                                                                          SetCubes([(2, Cube(Color.blue)), (1, Cube(Color.red)), (2, Cube(Color.green))])
+                                                                                          ])]))
+    def test_parse_string(cls, test_line, expected_results):
+        test_game = Game.parse_string(test_line)
+
+        assert test_game == expected_results
