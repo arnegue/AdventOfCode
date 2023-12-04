03468dab372bb5090d2969f8205c80a1ccde282f
Arne Güldener <arne-@hotmail.de>
2023-12-04 17:55:38 +0100

    Day4 part 2 working

--- 2023/fourth/game_card_evaluator.py
+++ 2023/fourth/game_card_evaluator.py
@@ -11,19 +11,28 @@ class Card:
 
     def evaluate_data(self):
         id_, rest = self.line.split(":")
+        self.id = int(next(re.finditer(r'\d+', id_)).group())
+
         possible_numbers, won_numbers = rest.split("|")
 
         self.winning_numbers = self.get_numbers_from_line(possible_numbers)
         self.own_numbers = self.get_numbers_from_line(won_numbers)
 
-    def get_winning_points(self):
-        temp_points = 0
+    def get_amount_winning_numbers(self) -> int:
+        amount_winning_numbers = 0
         for possible_number in self.own_numbers:
-            if possible_number in self.winning_numbers :
-                if temp_points == 0:
-                    temp_points = 1
-                else:
-                    temp_points *= 2
+            if possible_number in self.winning_numbers:
+                amount_winning_numbers += 1
+        return amount_winning_numbers
+
+    @classmethod
+    def get_winning_points(cls, amount_winning_numbers: int) -> int:
+        temp_points = 0
+        for i in range(amount_winning_numbers):
+            if temp_points == 0:
+                temp_points = 1
+            else:
+                temp_points *= 2
         return temp_points
 
     @classmethod
@@ -33,3 +42,29 @@ class Card:
             numbers.append(int(match.group()))
         return numbers
 
+
+class GameCard(Card):
+    def __init__(self, line: str):
+        super().__init__(line)
+        self.amount = 1
+
+
+class GameEvaluator(object):
+    def __init__(self, list_cards: []):
+        self.card_dict = {}
+        for card in list_cards:
+            self.card_dict[card.id] = card
+
+    def evaluate_wins(self):
+        for card_id, card in self.card_dict.items():
+            for _ in range(card.amount):
+                winning_numbers = card.get_amount_winning_numbers()
+                for o in range(winning_numbers):
+                    new_card_id = card.id + o + 1  # Plus one, we don't want to copy the own card
+                    self.card_dict[new_card_id].amount += 1
+
+    def get_amount_cards(self):
+        card_amount = 0
+        for card in self.card_dict.values():
+            card_amount += card.amount
+        return card_amount
--- 2023/fourth/test_game_card.py
+++ 2023/fourth/test_game_card.py
@@ -1,16 +1,16 @@
 import pytest
-from game_card_evaluator import Card
+from game_card_evaluator import Card, GameEvaluator, GameCard
 
 
 class TestPart1:
-    dest_data = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n" \
+    test_data = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n" \
                 "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\n" \
                 "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\n" \
                 "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\n" \
                 "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\n" \
                 "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
 
-    split_data = dest_data.splitlines(keepends=False)
+    split_data = test_data.splitlines(keepends=False)
 
     @pytest.mark.parametrize(("line", "expected_points"), [
         [split_data[0], 8],
@@ -23,7 +23,8 @@ class TestPart1:
     def test_game_data(self, line, expected_points):
         evaluator = Card(line)
         evaluator.evaluate_data()
-        actual_points = evaluator.get_winning_points()
+        winning_points = evaluator.get_amount_winning_numbers()
+        actual_points = evaluator.get_winning_points(winning_points)
         assert actual_points == expected_points
 
     def test_part_sum_test_data(self):  # For part 1 test
@@ -34,6 +35,39 @@ class TestPart1:
         for line in lines:
             evaluator = Card(line)
             evaluator.evaluate_data()
-            total_points += evaluator.get_winning_points()
+            winning_points = evaluator.get_amount_winning_numbers()
+            total_points += evaluator.get_winning_points(winning_points)
         print("Total Points:", total_points)
         assert total_points == 25571
+
+
+class TestPart2:
+    test_data = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n" \
+                "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\n" \
+                "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\n" \
+                "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\n" \
+                "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\n" \
+                "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+
+    split_data = test_data.splitlines(keepends=False)
+
+    def test_game_data(self):
+        cards = [GameCard(line) for line in self.split_data]
+        [card.evaluate_data() for card in cards]
+        evaluator = GameEvaluator(cards)
+        evaluator.evaluate_wins()
+
+        assert evaluator.get_amount_cards() == 30
+
+    def test_real_data(self):  # For part 1 test
+        with open("./test_data.txt", "r") as file:
+            lines = file.readlines()
+
+        cards = [GameCard(line) for line in lines]
+        [card.evaluate_data() for card in cards]
+        evaluator = GameEvaluator(cards)
+        evaluator.evaluate_wins()
+
+        amount = evaluator.get_amount_cards()
+        print(f"Amount cards: {amount()}")
+        assert amount == 8805731
